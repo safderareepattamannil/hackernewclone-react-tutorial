f1823ca125cfb8252afa267e1818fe496e64c1f3
Safder Areepattamannil <safderareepattamannil@gmail.com>
2021-02-12 10:03:28 -0500

    nav structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-        <h1>Hello World</h1>
-    </div>
-  );
+    return (
+        <div>
+            <a href="#"><img src="/assets/logo.gif" alt="hn logo"/></a>
+            <a href="#">Hacker News</a> 
+            <a href="#">new</a> | 
+            <a href="#">threads</a> | 
+            <a href="#">comments</a> |
+            <a href="#">ask</a> |
+            <a href="#">show</a> |
+            <a href="#">jobs</a> |
+            <a href="#">submit</a> 
+            <a href="#">login</a>
+        </div>
+    );
 }
-
 export default App;
