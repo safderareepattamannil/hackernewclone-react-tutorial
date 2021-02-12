6f9388c3b74e95910d0ca1875b80be5e876b3b44
Safder Areepattamannil <safderareepattamannil@gmail.com>
2021-02-12 12:36:16 -0500

    completed tutorial

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import "./App.css";
+import "./styles/Nav.css";
 
 function App() {
+
     return (
-        <div>
-            <a href="#"><img src="/assets/logo.gif" alt="hn logo"/></a>
-            <a href="#">Hacker News</a> 
-            <a href="#">new</a> | 
-            <a href="#">threads</a> | 
-            <a href="#">comments</a> |
-            <a href="#">ask</a> |
-            <a href="#">show</a> |
-            <a href="#">jobs</a> |
-            <a href="#">submit</a> 
-            <a href="#">login</a>
+        <div className="container">
+            <div className="navigation">
+                <a href="#">
+                    <img
+                        className="hn-logo"
+                        src="/assets/logo.gif"
+                        alt="hn logo"
+                    />
+                </a>
+                <a href="#" className={"bold title"}>
+                    Hacker News
+                </a>
+                <a href="#">new</a> |<a href="#">threads</a> |
+                <a href="#">comments</a> |<a href="#">ask</a> |
+                <a href="#">show</a> |<a href="#">jobs</a> |
+                <a href="#">submit</a>
+                <a href="#" className="login">
+                    login
+                </a>
+            </div>
         </div>
     );
 }
